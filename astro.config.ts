f19ef9107411e494f31530a41094bba243addf56
incognitotgt <tgt@incognitotgt.me>
2024-03-01 12:22:52 -0500

    ssr

--- astro.config.ts
+++ astro.config.ts
@@ -2,35 +2,45 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	redirects: {
-		'/guides': '/guides/intro/',
-	},
-	integrations: [
-		starlight({
-			favicon: "./src/assets/any-vm.png",
-			logo: {
-				src: "./src/assets/any-vm.png",
-			},
-			title: 'AnyCode Docs',
-			social: {
-				github: 'https://github.com/any-vm/anycode',
-				discord: "https://discord.gg/6cpcbKwjBn"
-			},
-			sidebar: [
-				{
-					label: 'Docs',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Introduction', link: '/guides/intro/' },
-						{ label: "Configuration", link: "/guides/config/" },
-						{ label: "Services", link: "/guides/services/" },
-					],
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+	site: "https://docs.anyvm.tech",
+  redirects: {
+    '/guides': '/guides/intro/'
+  },
+  integrations: [starlight({
+    favicon: "./src/assets/any-vm.png",
+    logo: {
+      src: "./src/assets/any-vm.png"
+    },
+    title: 'AnyCode Docs',
+    social: {
+      github: 'https://github.com/any-vm/anycode',
+      discord: "https://discord.gg/6cpcbKwjBn"
+    },
+    sidebar: [{
+      label: 'Docs',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Introduction',
+        link: '/guides/intro/'
+      }, {
+        label: "Configuration",
+        link: "/guides/config/"
+      }, {
+        label: "Services",
+        link: "/guides/services/"
+      }]
+    }],
+    customCss: ['./src/tailwind.css']
+  }), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
