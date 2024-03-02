628880f6103fa83b250385c3b84664c84810b2ce
incognitotgt <tgt@incognitotgt.me>
2024-03-01 19:29:35 -0500

    remove tailwind since it's pointless

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -35,8 +34,6 @@ export default defineConfig({
         autogenerate: { directory: 'reference' },
       },
     ],
-    customCss: ['./src/tailwind.css']
-  }), tailwind({
-    applyBaseStyles: false
+    customCss: ['./src/index.css']
   })],
 });
